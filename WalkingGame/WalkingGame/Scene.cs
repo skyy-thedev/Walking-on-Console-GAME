24ec57477d1f3b3f6fdb1b4aa0db5a066de79687
skyy_dev <skyzeduardo@gmail.com>
2023-08-29 14:07:53 -0300

    modified

--- WalkingGame/WalkingGame/Scene.cs
+++ WalkingGame/WalkingGame/Scene.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace WalkingGame
 {
-    internal class Scene
+     class Scene
     {
+        public String Floor { set; get; }
+
+        public Byte Size { set; get; }  
+
+        public Scene(String floor) 
+        {
+            this.Floor = floor;
+            this.Size = size;
+        }
+
+        public void print(Player p)
+        {
+            Console.Clear();
+            Console.WriteLine();
+
+            // PRINT FIRST LINE //
+            if (p.Jumping)
+            {
+                for (Byte i = 0; i  p.Position; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(p.Skin);
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+
+            // PRINT SECOND LINE //
+            for (Byte i = 0; i < p.Position; i++)
+            {
+                Console.Write(Floor);
+            }
+
+            if (p.Jumping) Console.Write(Floor);
+            else Console.Write(p.Skin);
+
+            for (Byte i = 0; i < (Size - p.Position); i++)
+            {
+                Console.Write(Floor);
+            }
+        }
     }
 }
