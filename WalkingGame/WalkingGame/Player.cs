beb0d6c3a63cf5c3e6eef46cd0b46822dbf2908b
skyy_dev <skyzeduardo@gmail.com>
2023-08-29 14:21:42 -0300

    modified

--- WalkingGame/WalkingGame/Player.cs
+++ WalkingGame/WalkingGame/Player.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace WalkingGame
 {
-    internal class Player
+     class Player
     {
+        public Byte Position { set; get; }
+        public String Skin { set; get; }
+        public String FacingRight { set; get; } 
+        public String FacingLeft { set; get;}
+        public Boolean Jumping { set; get; }    
+
+        public Player(String right, String left) 
+        {
+            Position = 0;
+            FacingRight = right;
+            FacingLeft = left;
+            Skin = FacingRight;
+            Jumping = false;
+        }
+
+        public void jump()
+        {
+            Console.Beep(234, 100);
+        }
+
+        public void walk(Boolean direction, Byte limit)
+        {
+            if (direction)
+            {
+                if (Position < limit)
+                {
+                    Position++;
+                    Skin = FacingRight;
+                }
+            }
+            else
+            {
+                if (Position > 0)
+                {
+                    Position--;
+                    Skin = FacingLeft;
+                }
+            }
+        }
     }
 }
